be8f2439b8192fb528a8a18eb3aa720e38e850a0
ooyang0325 <diego930325@gmail.com>
2024-03-21 01:00:39 +0800

    BREAKING CHANGE: finished 3rd problem

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+import {Test, console} from "forge-std/Test.sol";
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    bool public isEnrolled;
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(!isEnrolled) {
+            isEnrolled = true;
+            return 1000;
+        }
+        return 123;
     }
 }
 
@@ -18,6 +23,10 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(IClassroomV2(msg.sender).isEnrolled() == false) {
+            return 1000;
+        }
+        return 123;
     }
 }
 
@@ -25,5 +34,10 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        //print(gasleft());=
+        if(gasleft() >= 7000){
+            return 1000;
+        }
+        return 123;
     }
 }
